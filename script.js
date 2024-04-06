0d2ac0f230f9a07a066ed40d25374dc7101a7bd9
Duc Tai TRAN <tdts2010@gmail.com>
2024-04-06 21:49:36 +0200

    test

--- script.js
+++ script.js
@@ -63,3 +63,35 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
   });
 });
+
+
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 20;
+    this.height = 20;
+    this.color = 'green';
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+const player = new Player(canvas.width / 2, canvas.height - 40);
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  requestAnimationFrame(animate);
+}
+
+animate();
