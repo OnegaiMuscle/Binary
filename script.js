a15af1208f2931c111a5bb74eed61084a0fa6176
Duc Tai TRAN <tdts2010@gmail.com>
2024-04-09 01:29:34 +0200

    test

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function displayStatus(e) {
-  console.log(e)
-  console.log(e.children[0].checked)
   e.children[1].textContent = e.children[0].checked ? 1 : 0
 
 }
 
+function display(t, e) {
 
+  console.log(t)
+  console.log(e)
+  console.log(e.target.parentElement)
+  console.log(e.target.checked)
+  console.log([...t.children].indexOf(e.target.parentElement))
+
+  console.log(e.target.nextElementSibling)
+  e.target.nextElementSibling.textContent= e.target.checked ? 1 : 0
+  const z = t.children
+  console.log(parseInt([...t.children].map(x=>x.children[1].textContent).join(""),2
+  ))
+
+
+  }
